abc2f56a697496081786c5f2f85b6924dc5a1113
hanyouhong <python@sina.cn>
2017-07-19 01:03:32 +0800

    chinese translation

--- src/locales/zh.js
+++ src/locales/zh.js
@@ -1,1 +1,147 @@
-export default {};
+export default {
+    header: {
+        navigation: {
+            distribution: '发行',
+            whitepapers: '白皮书',
+            downloads: '下载',
+            explorer: '区块浏览',
+            blog: '博客',
+            buy: '购买Skycoin',
+        },
+    },
+    footer: {
+        getStarted: '开始',
+        explore: '浏览',
+        community: '社区',
+        wallet: '下载钱包',
+        buy: '购买Skycoin',
+        infographics: '信息图表',
+        whitepapers: '白皮书',
+        blockchain: '区块链',
+        distribution: '发行事件',
+        blog: '博客',
+        github: 'Github',
+        telegram: 'Telegram',
+        slack: 'Slack',
+    },
+    buyBanner: {
+        lead: '可以在下面的交易所购买Skycoins',
+    },
+    buy: {
+        heading: '选择你喜欢的交易所购买Skycoin',
+        c2cx: '在C2CX购买',
+        cryptopia: '在Cryptopia购买',
+    },
+    notFound: {
+        title: '页面没有找到',
+        heading: '页面没有找到',
+        home: 'Skycoin首页',
+    },
+    getStarted: {
+        heading: '开始使用Skycoin吧',
+        buy: '购买Skycoin',
+        wallet: '下载钱包',
+    },
+    home: {
+        title: '构建新互联网的基础框架',
+        description: 'Skycoin被设计用来实现中本聪最初的愿景,并且修复比特币存在的已知问题.',
+        hero: {
+            heading: '构建新互联网{break}的基础框架.',
+            buy: '购买Skycoin',
+            wallet: {
+                get: '下载钱包',
+                blurb: '钱包让你方便快捷的拥有Skycoin.{break}它快速,安全并且免费.',
+            },
+            announcement: '在7月24,1%的Skycoin(1百万个)将以0.002 BTC / SKY的固定价格出售',
+        },
+        about: {
+            heading: '什么是Skycoin?',
+            lead: 'Skycoin是设计用来实现中本聪最初的愿景,并且修复比特币存在的已知问题.',
+            whitepaper: '白皮书',
+            speed: {
+                heading: '快速',
+                body: '我们构建Skycoin来和信用卡,支付宝,苹果支付等竞争.交易可以在数秒而不是数分钟内完成.',
+            },
+            privacy: {
+                heading: '隐私',
+                body: 'Skycoin通过使用CoinJoin协议来混合多个钱包的交易来提高隐私,使追踪交易变得不可能. ',
+            },
+            security: {
+                heading: '安全',
+                body: 'Skycoin更加安全,因为它不依赖旷工挖矿,不可能遭受51%攻击.',
+            },
+            ecosystem: {
+                heading: '生态',
+                body: '我们正在构建一个丰富的真实应用生态,开始的时候在Skycoin生态里面就有消息,社交媒体和VPN应用.',
+            },
+        },
+        network: {
+            label: '深入了解',
+            heading: '革命性的新共识算法',
+            body: '中本聪最初设计是让比特币网络在几百万台计算机上分布式的工作. 现在,比特币网络被几个矿池完全控制,比特币不再是去中心化系统. Skycoin的共识算法被设计来达到真正的去中心化区块共识',
+            link: '深入了解网络共识',
+        },
+        miner: {
+            label: '即将发售',
+            heading: 'Skywire矿机: 为新互联网准备的硬件',
+        },
+        roadmap: {
+            heading: '路线图',
+            blurb: 'Skycoin的开发可以通过关注GitHub的项目来参与. 在接下来的几个月我们将为Bug修正,设计和开发提供奖励.',
+            roadmapLink: '查看完整的路线图',
+            community: '为了跟进Skycoin的最新开发进展或者开始做贡献,请加入在Telegram的Skycoin频道.',
+            communityLink: '加入社区',
+            timeline: {
+                network: {
+                    quarter: 'Q3',
+                    year: '2017',
+                    content: '启动第一代可下载的网络节点,用户可以通过提供网络支持节点来赚取Skycoin.',
+                },
+                applications: {
+                    quarter: 'Q3',
+                    year: '2017',
+                    content: '启动在Skywire网络上第一批应用: 去中心化的,类似Reddit的社交平台和VPN服务.',
+                },
+            },
+        },
+    },
+    downloads: {
+        title: '下载',
+        description: '下载Skycoin钱包和白皮书.',
+        wallet: {
+            heading: '钱包',
+            lead: '想要容易使用,下载桌面钱包. 针对高级用户或慢速连接, 命令行(CLI)钱包提供了一个可以直接在浏览器运行的UI界面',
+            build: '当前发布版本是 {version}.',
+            macos: 'MacOS',
+            linux: 'Linux',
+            windows: 'Windows',
+            withElectron: '桌面钱包',
+            withoutElectron: 'CLI钱包',
+            download: '下载',
+            torrent: '种子',
+        },
+        whitepapers: {
+            heading: '白皮书',
+            download: '下载',
+        },
+    },
+    infographics: {
+        title: '信息图表',
+    },
+    distribution: {
+        title: 'Skycoin发行事件',
+        description: '在7月24,1%的Skycoin(1百万个)将以0.002 BTC / SKY的固定价格出售.',
+        hero: {
+            heading: 'Skycoin发行事件',
+            lead: '在7月24,1%的Skycoin(1百万个)将以0.002 BTC / SKY的固定价格(即1个比特币换500个Skycoin)出售.这次发行事件会持续到8月8号或者100万的天空币被卖光.',
+            body: '访问skycoin.net网站来参加发行.',
+        },
+        countdown: {
+            heading: '北京时间<strong>7月24晚上8点</strong>开始发行',
+            days: '天',
+            hours: '小时',
+            minutes: '分钟',
+            seconds: '秒',
+        },
+    },
+};

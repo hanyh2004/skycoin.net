e923fe073d4e20647d468e749ddaaaaf892f4c56
hanyouhong <python@sina.cn>
2017-07-19 14:48:48 +0800

    chinese translation fixed 	modified:   ../config.js 	modified:   zh.js

--- src/locales/zh.js
+++ src/locales/zh.js
@@ -1,147 +1,147 @@
 export default {
-    header: {
-        navigation: {
-            distribution: '发行',
-            whitepapers: '白皮书',
-            downloads: '下载',
-            explorer: '区块浏览',
-            blog: '博客',
-            buy: '购买Skycoin',
-        },
+  header: {
+    navigation: {
+      distribution: '币分发',
+      whitepapers: '白皮书',
+      downloads: '下载区',
+      explorer: '区块浏览器',
+      blog: '开发日志',
+      buy: '购买天空币',
     },
-    footer: {
-        getStarted: '开始',
-        explore: '浏览',
-        community: '社区',
-        wallet: '下载钱包',
-        buy: '购买Skycoin',
-        infographics: '信息图表',
-        whitepapers: '白皮书',
-        blockchain: '区块链',
-        distribution: '发行事件',
-        blog: '博客',
-        github: 'Github',
-        telegram: 'Telegram',
-        slack: 'Slack',
+  },
+  footer: {
+    getStarted: '开始',
+    explore: '浏览',
+    community: '社区',
+    wallet: '下载钱包',
+    buy: '购买天空币',
+    infographics: '信息图表',
+    whitepapers: '白皮书',
+    blockchain: '区块浏览器',
+    distribution: '分发活动',
+    blog: '开发日志',
+    github: 'Github',
+    telegram: 'Telegram',
+    slack: 'Slack',
+  },
+  buyBanner: {
+    lead: '选择以下交易所购买天空币',
+  },
+  buy: {
+    heading: '选择以下交易所购买天空币',
+    c2cx: '前往C2CX购买',
+    cryptopia: '前往Cryptopia购买',
+  },
+  notFound: {
+    title: '页面没有找到',
+    heading: '页面没有找到',
+    home: '天空币首页',
+  },
+  getStarted: {
+    heading: '开始天空币之旅',
+    buy: '购买天空币',
+    wallet: '下载钱包',
+  },
+  home: {
+    title: '下一代互联网的基石',
+    description: '天空币的设计是用来实现中本聪对比特币的原初规划，并解决比特币的现存问题。',
+    hero: {
+      heading: '下一代互联网的基石',
+      buy: '购买天空币',
+      wallet: {
+        get: '下载钱包',
+        blurb: '钱包让您安全地持有和管理天空币资产。{break}快速、安全、免费。',
+      },
+      announcement: '7月24日,1%(1百万枚)的天空币将以0.002 BTC / SKY的固定价格出售',
     },
-    buyBanner: {
-        lead: '可以在下面的交易所购买Skycoins',
+    about: {
+      heading: '什么是天空币?',
+      lead: '天空币的设计是用来实现中本聪对比特币的原初规划，并解决比特币的现存问题。',
+      whitepaper: '白皮书',
+      speed: {
+        heading: '快速',
+        body: '我们着力构建足以匹敌信用卡、支付宝或Apple Pay的支付方案。交易速度将从分钟级别降至秒付。',
+      },
+      privacy: {
+        heading: '隐私',
+        body: '天空币使用CoinJoin协议，通过多钱包的混合交易记录方式，实现交易不可被追踪，隐私保障更进一步。',
+      },
+      security: {
+        heading: '安全',
+        body: '天空币更加安全可靠，不再将网络安全托付给矿工，排除了51%攻击的可能。',
+      },
+      ecosystem: {
+        heading: '生态',
+        body: '我们正在搭建丰富多样的天空币生态系统来满足现实需求，首先推出即时通信、社交媒体和VPN。',
+      },
     },
-    buy: {
-        heading: '选择你喜欢的交易所购买Skycoin',
-        c2cx: '在C2CX购买',
-        cryptopia: '在Cryptopia购买',
+    network: {
+      label: '深入了解',
+      heading: '革命性的全新共识算法',
+      body: '中本聪的设计,原本是要通过去大量中心化的计算机来控制比特币网络。现如今，比特币网络完全被三家矿池控制，比特币无法再以去中心化自居。天空币的共识算法才可以实现真正的区块链共识去中心化的设计。',
+      link: '深入了解网络共识',
     },
-    notFound: {
-        title: '页面没有找到',
-        heading: '页面没有找到',
-        home: 'Skycoin首页',
+    miner: {
+      label: '即将推出',
+      heading: '天空链矿机：驱动下一代互联网的硬件',
     },
-    getStarted: {
-        heading: '开始使用Skycoin吧',
-        buy: '购买Skycoin',
-        wallet: '下载钱包',
-    },
-    home: {
-        title: '构建新互联网的基础框架',
-        description: 'Skycoin被设计用来实现中本聪最初的愿景,并且修复比特币存在的已知问题.',
-        hero: {
-            heading: '构建新互联网{break}的基础框架.',
-            buy: '购买Skycoin',
-            wallet: {
-                get: '下载钱包',
-                blurb: '钱包让你方便快捷的拥有Skycoin.{break}它快速,安全并且免费.',
-            },
-            announcement: '在7月24,1%的Skycoin(1百万个)将以0.002 BTC / SKY的固定价格出售',
-        },
-        about: {
-            heading: '什么是Skycoin?',
-            lead: 'Skycoin是设计用来实现中本聪最初的愿景,并且修复比特币存在的已知问题.',
-            whitepaper: '白皮书',
-            speed: {
-                heading: '快速',
-                body: '我们构建Skycoin来和信用卡,支付宝,苹果支付等竞争.交易可以在数秒而不是数分钟内完成.',
-            },
-            privacy: {
-                heading: '隐私',
-                body: 'Skycoin通过使用CoinJoin协议来混合多个钱包的交易来提高隐私,使追踪交易变得不可能. ',
-            },
-            security: {
-                heading: '安全',
-                body: 'Skycoin更加安全,因为它不依赖旷工挖矿,不可能遭受51%攻击.',
-            },
-            ecosystem: {
-                heading: '生态',
-                body: '我们正在构建一个丰富的真实应用生态,开始的时候在Skycoin生态里面就有消息,社交媒体和VPN应用.',
-            },
-        },
+    roadmap: {
+      heading: '路线图',
+      blurb: '可以在Github上跟进开发进程。我们即将启动奖励机制，以便奖励那些提供漏洞修复、设计、或其他工作的社区成员。',
+      roadmapLink: '查看完整的路线图',
+      community: '加入天空币Telegram群，持续跟进开发动态，为社区出力。',
+      communityLink: '加入社区',
+      timeline: {
         network: {
-            label: '深入了解',
-            heading: '革命性的新共识算法',
-            body: '中本聪最初设计是让比特币网络在几百万台计算机上分布式的工作. 现在,比特币网络被几个矿池完全控制,比特币不再是去中心化系统. Skycoin的共识算法被设计来达到真正的去中心化区块共识',
-            link: '深入了解网络共识',
+          quarter: '3季度',
+          year: '2017年度',
+          content: '开启可供用户下载的网络节点。用户可以通过驱动网络赚取天空币。',
         },
-        miner: {
-            label: '即将发售',
-            heading: 'Skywire矿机: 为新互联网准备的硬件',
+        applications: {
+          quarter: '3季度',
+          year: '2017年度',
+          content: '天空链网络应用首发：去中心化类Reddit论坛的社交媒体平台及VPN服务。',
         },
-        roadmap: {
-            heading: '路线图',
-            blurb: 'Skycoin的开发可以通过关注GitHub的项目来参与. 在接下来的几个月我们将为Bug修正,设计和开发提供奖励.',
-            roadmapLink: '查看完整的路线图',
-            community: '为了跟进Skycoin的最新开发进展或者开始做贡献,请加入在Telegram的Skycoin频道.',
-            communityLink: '加入社区',
-            timeline: {
-                network: {
-                    quarter: 'Q3',
-                    year: '2017',
-                    content: '启动第一代可下载的网络节点,用户可以通过提供网络支持节点来赚取Skycoin.',
-                },
-                applications: {
-                    quarter: 'Q3',
-                    year: '2017',
-                    content: '启动在Skywire网络上第一批应用: 去中心化的,类似Reddit的社交平台和VPN服务.',
-                },
-            },
-        },
+      },
     },
-    downloads: {
-        title: '下载',
-        description: '下载Skycoin钱包和白皮书.',
-        wallet: {
-            heading: '钱包',
-            lead: '想要容易使用,下载桌面钱包. 针对高级用户或慢速连接, 命令行(CLI)钱包提供了一个可以直接在浏览器运行的UI界面',
-            build: '当前发布版本是 {version}.',
-            macos: 'MacOS',
-            linux: 'Linux',
-            windows: 'Windows',
-            withElectron: '桌面钱包',
-            withoutElectron: 'CLI钱包',
-            download: '下载',
-            torrent: '种子',
-        },
-        whitepapers: {
-            heading: '白皮书',
-            download: '下载',
-        },
+  },
+  downloads: {
+    title: '下载',
+    description: '下载天空币钱包和白皮书.',
+    wallet: {
+      heading: '钱包',
+      lead: '为使用简便，请下载安装桌面钱包。针对高级用户或慢速连接, 命令行钱包提供了一个可以直接在浏览器运行的UI界面',
+      build: '当前发布版本是 {version}.',
+      macos: 'MacOS',
+      linux: 'Linux',
+      windows: 'Windows',
+      withElectron: '桌面版钱包',
+      withoutElectron: '命令行钱包',
+      download: '下载',
+      torrent: '种子',
     },
-    infographics: {
-        title: '信息图表',
+    whitepapers: {
+      heading: '白皮书',
+      download: '下载',
     },
-    distribution: {
-        title: 'Skycoin发行事件',
-        description: '在7月24,1%的Skycoin(1百万个)将以0.002 BTC / SKY的固定价格出售.',
-        hero: {
-            heading: 'Skycoin发行事件',
-            lead: '在7月24,1%的Skycoin(1百万个)将以0.002 BTC / SKY的固定价格(即1个比特币换500个Skycoin)出售.这次发行事件会持续到8月8号或者100万的天空币被卖光.',
-            body: '访问skycoin.net网站来参加发行.',
-        },
-        countdown: {
-            heading: '北京时间<strong>7月24晚上8点</strong>开始发行',
-            days: '天',
-            hours: '小时',
-            minutes: '分钟',
-            seconds: '秒',
-        },
+  },
+  infographics: {
+    title: '信息图表',
+  },
+  distribution: {
+    title: '天空币分发活动',
+    description: '在7月24,1%(1百万枚)的天空币将以0.002 BTC / SKY的固定价格出售.',
+    hero: {
+      heading: '天空币分发活动',
+      lead: '7月24日开始，总量1%（1百万枚）的天空币会以单价0.002BTC的价格售出（即1BTC=500SKY）。此次分发活动8月8日前一百万枚天空币售罄后即刻截止，若8月8日未售罄则8月8日截止。',
+      body: '回到skycoin.net参与分发活动。',
     },
+    countdown: {
+      heading: '北京时间<strong>7月24晚上8点</strong>开始发行',
+      days: '天',
+      hours: '小时',
+      minutes: '分钟',
+      seconds: '秒',
+    },
+  },
 };
